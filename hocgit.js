a315e61f93e98638294732b90b7ddd4db387bd42
vanquan2k <anhlapro1243@gmail.com>
2022-05-18 11:15:35 +0700

    quan

--- hocgit.js
+++ hocgit.js
@@ -12,5 +12,31 @@
 // git add .
 // git commit --amend :wq
 // git push origin "ten nhanh" -f
+// sau do click vao merger de lay file tu nhanh len master
+// de lay code moi nhat tu nhanh master ve 
+//git checkout master
+// git pull origin master de lay code moi nhat ve vscode
+// git branch -D "tennhanh"
+
+
+
+
 
 console.log('hoc ve git')
+
+
+
+// thanh vien trong du an 
+// b1 : fork cai repo chinh
+// B2 : git clone cai du an ve
+// b3 : git remote - v de kiem tra
+// b4 : git remote add "tencongty" "url"
+// b5 : git checkout -b " ten nhanh moi"
+// b6 : giong nhu tren git add va git commit
+// b7 : git push origin "ten"
+ // b8 : git commit --amend , git push origin "ten nhanh " -f
+
+
+ // b9 ; cty merge
+ //b10 : git pull "tencty" main
+ // git branch -D " ten nhanh" ; de xoa
