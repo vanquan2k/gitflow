2fbc24da98e50a8146f28891d79da943f432d337
vanquan2k <anhlapro1243@gmail.com>
2022-05-18 10:03:05 +0700

    add

---
+++ add.js
@@ -0,0 +1,5 @@
+console.log('aaa')
+console.log('tesst lai pull request')
+
+
+
---
+++ hocgit.js
@@ -0,0 +1,16 @@
+
+// hoc git
+// chu du an 
+// cach 1: tao repo xong clone
+// cach 2 : commit xong roi push
+// tao nhanh moi khac voi master :git checkout -b "ten nhanh"
+// git add .
+// git commit -m "noi dung"
+// git push origin "ten nhanh"
+// tao pull request : tren git 
+// khi muon sua code o nhanh xong oush lai thi 
+// git add .
+// git commit --amend :wq
+// git push origin "ten nhanh" -f
+
+console.log('hoc ve git')
